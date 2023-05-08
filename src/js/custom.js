77c0b7bfa29a8e8e8d1d5fb8ed3b84f3531f8dbf
kolyasademetrio <kolyasademetrio@gmail.com>
2023-05-08 23:19:45 +0300

    Popup + sticky header

--- src/js/custom.js
+++ src/js/custom.js
@@ -60,4 +60,40 @@ $(document).ready(() => {
   $('.js-mask').inputmask({
     mask: "99 - 99 - 99",
   });
+
+  const btns = document.querySelectorAll('.js-popup-open');
+  const btnClose = document.querySelector('.js-popup-close');
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+      document.querySelector('.js-popup').classList.add('active');
+      document.body.classList.add('withPopup');
+    });
+  });
+
+  btnClose.addEventListener('click', e => {
+    document.querySelector('.js-popup').classList.remove('active');
+    document.body.classList.remove('withPopup');
+  });
+
+
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function () {
+    myFunction()
+  };
+
+  // Get the header
+  var header = document.querySelector(".js-sticky");
+
+  // Get the offset position of the navbar
+  var sticky = header.offsetTop;
+
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
 });
